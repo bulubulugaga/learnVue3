fd730a45d675555da0771002f0854122a8f2de2d
DESKTOP-MBAI5QL\zwzh <2356226565@qq.com>
2022-01-12 15:46:58 +0800

    reactive函数

--- notes/02常用CompositionApi.md
+++ notes/02常用CompositionApi.md
@@ -1,6 +1,6 @@
 > 官网：https://v3.cn.vuejs.org/guide/composition-api-introduction.html   
 
-# 拉开序幕的setup
+# 1. 拉开序幕的setup
 1. 理解：vue3中的一个新的配置项，值为一个函数。
 2. setup是所有Composition API(组合API)“表演的舞台”。
 3. 组件中所有用到的：数据、方法等，均配置在setup中。
@@ -14,7 +14,7 @@
         * 如果重名，看顺序，data在前以data为主，setup在前以setup为主。
     2. setup不能是一个async函数，因为返回值不再是return的对象，而是promise，模板中看不到return对象中的属性。
 
-# ref函数
+# 2. ref函数
 1. 作用：定义一个响应式数据。
 2. 语法：const xxx = ref(initValue)。
     * 创建一个包含响应式数据的引用对象（reference对象）。
@@ -24,3 +24,9 @@
     * 接收的数据可以是：基本类型、也可以是对象类型。
     * 基本类型数据：响应式依然是依靠Object.defineProperty的get和set。
     * 对象类型的数据：内部“求助”vue3中的新函数----reactive函数。
+
+# 3. reactive函数
+* 作用：定义一个对象类型的响应式数据（基本类型不要用它，用ref函数）。
+* 语法：const代理对象 = reactive(源对象)接收一个对象或数组，返回一个代理对象(proxy对象)。
+* reactive定义的响应式数据是“深层次的”。
+* 内部基于ES6的Proxy实现，通过代理对象操作源对象内部数据进行操作。
--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,80 @@
   <h2>年龄：{{ age }}</h2>
   <h2>工作：{{ job.type }}</h2>
   <h2>薪资：{{ job.salary }}</h2>
-  <h2>数组：{{ arr }}</h2>
+  <h2>深层嵌套测试：{{ job.a.b.c }}</h2>
+  <h2>爱好：{{ hobbies }}</h2>
   <button @click="changeInfo">修改信息</button>
+  <br />
+  <br />
+  <h1>------简化之后----------------</h1>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年龄：{{ person.age }}</h2>
+  <h2>工作：{{ person.job.type }}</h2>
+  <h2>薪资：{{ person.job.salary }}</h2>
+  <h2>深层嵌套测试：{{ person.job.a.b.c }}</h2>
+  <h2>爱好：{{ person.hobbies }}</h2>
+  <button @click="changeInfo2">修改信息</button>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 export default {
   name: 'App',
-  // 此处只是测试setup，暂时不考虑响应式问题
   setup() {
     let name = ref('张三');
     let age = ref(18);
-    let job = ref({
+    let job = reactive({
       type: '前端工程师',
-      salary: '30k'
+      salary: '30k',
+      a: {
+        b: {
+          c: 666
+        }
+      }
     });
-    let arr = ref([1, 2, 3]);
+    let hobbies = reactive(['抽烟', '喝酒', '烫头']);
     
     function changeInfo() {
       name.value = '李四';
       age.value = 48;
-      job.value.type = '后端工程师';
-      arr.value[1] = 20;
-
-      console.log(name);   // RefImpl{..., value}    get和set实现响应式
-      console.log(job.value);   // Proxy{type, value}    proxy实现响应式
+      job.type = '后端工程师';
+      job.a.b.c = 999;
+      hobbies[0] = '学习';
     } 
+
+    // 简化
+    let person = reactive({
+      name: '张三',
+      age: 18,
+      job: {
+        type: '前端工程师',
+        salary: '30k',
+        a: {
+          b: {
+            c: 666
+          }
+        }
+      },
+      hobbies: ['抽烟', '喝酒', '烫头']
+    })
+    function changeInfo2() {
+      person.name = '李四';
+      person.age = 48;
+      person.job.type = '后端工程师';
+      person.job.a.b.c = 999;
+      person.hobbies[0] = '学习';
+    }
 
     return {
       name, 
       age,
       job,
-      arr,
-      changeInfo
+      hobbies,
+      changeInfo,
+
+      person,
+      changeInfo2
     }
   }
 }
