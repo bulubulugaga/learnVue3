d642f337b95d4d5d3d6c618917e3850ed16a684e
DESKTOP-MBAI5QL\zwzh <2356226565@qq.com>
2022-01-21 17:55:38 +0800

    toRaw和markRaw

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,12 +1,17 @@
 <template>
   <h2>一个人的信息是：</h2>
+  <h2>{{ person }}</h2>
   <h2>姓名：{{ name }}</h2>
   <h2>年龄：{{ age }}</h2>
   <h2>薪资：{{ job.j1.salary }}</h2>
+  <h2>车辆：{{ person.car }}</h2>
 
-  <button @click="name+='~'">修改姓名</button><br />
+  <button @click="name+='~';">修改姓名</button><br />
   <button @click="age+=1">修改年龄</button><br />
-  <button @click="job.j1.salary+=1">修改薪资</button>
+  <button @click="job.j1.salary+=1">修改薪资</button><br />
+  <button @click="showRowPerson">输出最原始的person</button><br />
+  <button @click="addCar">增加车辆信息</button><br />
+  <button v-if="person.car" @click="changeCar">修改车辆名字</button><br />
 
   <hr />
 
@@ -15,7 +20,7 @@
 </template>
 
 <script>
-import { ref, reactive, toRefs, readonly, shallowReadonly } from 'vue'
+import { ref, reactive, toRefs, toRaw, markRaw } from 'vue'
 export default {
   name: 'Demo',
   setup() {
@@ -30,14 +35,39 @@ export default {
     })
     let sum = ref(0);
 
-    sum = readonly(sum);
-    // sum = shallowReadonly(sum);   // 因为里面就是基本数据类型，所以跟readonly效果一样
-    // person = readonly(person);
-    person = shallowReadonly(person);   // 可修改salary
+    function showRowPerson() {
+      // console.log(person);   // Proxy{...}
 
+      const p = toRaw(person);
+      p.age++;    // person的age也改变了，只是页面没有刷新，当响应式改变person值时，之前的age就会更新，与shallowRactive一样
+      console.log(p, person);    // {}, Proxy{}
+
+      p.job = markRaw(p.job);
+
+      const s = toRaw(sum);
+      // s++;  // 报错，s is readonly
+      console.log(s, sum);   // RefImpl, RefImpl
+
+    }
+
+    function addCar() {
+      const car = { name: '宝马', money: '20w' }
+      // person.car = car;
+      person.car = markRaw(car);
+    }
+
+    function changeCar() {
+      person.car.name+='!';    // 数据改变，没有响应
+      console.log(person.car);
+    }
+    
     return {
       sum, 
-      ...toRefs(person) 
+      person,
+      ...toRefs(person),
+      showRowPerson,
+      addCar,
+      changeCar
     }
   }
 }
